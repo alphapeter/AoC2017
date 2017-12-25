f965597f98179d34c98a42e80eb76008cc98bb93
Peter <peter@otocolobus.se>
2017-12-25 17:34:42 +0100

    day 10

--- day10.py
+++ day10.py
@@ -1,3 +1,4 @@
+import binascii
 def calc_a(input, length):
 
     k_hash = list(range(0, length))
@@ -7,14 +8,12 @@ def calc_a(input, length):
     for knot_length in input:
         start = current_position
         end = current_position + min(knot_length, length)
-        print('start', start, 'end', end)
         if end > length:
             reversed = reverse(k_hash[start:] + k_hash[:(end % length)])
             k_hash = reversed[abs(start - length):] + k_hash[(end % length):start] + reversed[:abs(start - length)]
         else:
             k_hash = k_hash[:start] + reverse(k_hash[start:end]) + k_hash[end:]
 
-        print(k_hash)
         current_position = (current_position + skip_size + knot_length) % length
         skip_size += 1
 
@@ -25,6 +24,33 @@ def reverse(lst):
     lst.reverse()
     return lst
 
-def calc_b(input):
-    return 0
+def calc_b(input, length):
+
+    key = list(map(lambda c: ord(c), input)) + [17, 31, 73, 47, 23]
+    k_hash = list(range(0, length))
+    current_position = 0
+    skip_size = 0
+    for _ in range(64):
+        for knot_length in key:
+            start = current_position
+            end = current_position + min(knot_length, length)
+            if end > length:
+                reversed = reverse(k_hash[start:] + k_hash[:(end % length)])
+                k_hash = reversed[abs(start - length):] + k_hash[(end % length):start] + reversed[:abs(start - length)]
+            else:
+                k_hash = k_hash[:start] + reverse(k_hash[start:end]) + k_hash[end:]
+
+            current_position = (current_position + skip_size + knot_length) % length
+            skip_size += 1
+
+    a = []
+
+    for i in range(16):
+        offset = i * 16
+        c = k_hash[offset]
+        for j in range(1, 16):
+            c = c ^ k_hash[offset + j]
+        a.append(c)
+    return ''.join('%.2x' % x for x in a)
+
 
--- test_day10.py
+++ test_day10.py
@@ -2,7 +2,8 @@ import day10
 import unittest
 
 
-input = 83, 0, 193, 1, 254, 237, 187, 40, 88, 27, 2, 255, 149, 29, 42, 100
+input_a = 83, 0, 193, 1, 254, 237, 187, 40, 88, 27, 2, 255, 149, 29, 42, 100
+input_b = "83,0,193,1,254,237,187,40,88,27,2,255,149,29,42,100"
 
 
 class TestDay10a(unittest.TestCase):
@@ -20,10 +21,19 @@ class TestDay10a(unittest.TestCase):
         self.assertEqual(12, day10.calc_a((3, 4, 1, 5), 5))
 
     def test_input(self):
-        self.assertEqual(20056, day10.calc_a(input, 256))
+        self.assertEqual(20056, day10.calc_a(input_a, 256))
 
 
 class TestDay10b(unittest.TestCase):
 
-    def test_case(self):
-       self.assertEqual(6622, day10.calc_b(input))
+    def test_case_1(self):
+       self.assertEqual('a2582a3a0e66e6e86e3812dcb672a272', day10.calc_b('', 256))
+    def test_case_2(self):
+       self.assertEqual('33efeb34ea91902bb2f59c9920caa6cd', day10.calc_b('AoC 2017', 256))
+    def test_case_3(self):
+       self.assertEqual('3efbe78a8d82f29979031a4aa0b16a9d', day10.calc_b('1,2,3', 256))
+    def test_case_4(self):
+       self.assertEqual('63960835bcdc130f0b66d7ff4f6a5a8e', day10.calc_b('1,2,4', 256))
+
+    def test_case_4(self):
+       self.assertEqual('d9a7de4a809c56bf3a9465cb84392c8e', day10.calc_b(input_b, 256))
